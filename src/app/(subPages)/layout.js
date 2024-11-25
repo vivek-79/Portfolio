a3442ed3111b98a4f9d536314d9a175825dec652
vivek-79 <vivekroy5667@gmail.com>
2024-11-25 23:24:22 +0530

    up-3

--- src/app/(subPages)/layout.js
+++ src/app/(subPages)/layout.js
@@ -1,6 +1,6 @@
 import HomeBtn from "../comps/HomeBtn";
 
-export default function({ children }) {
+export default function Layout({ children }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-8 xs:16 py-20 lg:px-32">
         <HomeBtn/>
